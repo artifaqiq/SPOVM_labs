2dc1569ba79f5d55229f00f0b54ff19308d30bb1
artifaqiq <artifaqiq@gmail.com>
2016-03-20 13:30:51 +0300

    laba2

---
+++ lab2/printpidprog.cpp
@@ -0,0 +1,171 @@
+#include <stdlib.h>
+#include <stdio.h>
+#ifdef  __linux__
+#include <unistd.h>
+#include <signal.h>
+#elif __WIN32
+#include <windows.h>
+#include <iostream>
+#endif
+void printMessage();
+#ifdef  __linux__
+
+void emptyHandle(int signo, siginfo_t* info, void* context){}
+
+void sigTermHandle(int signo, siginfo_t *info, void *context)
+{
+  lockf(fileno(stdout), F_LOCK, 100);
+  printf("Terminated process [ pid = %d ]\n", getpid());
+  lockf(fileno(stdout), F_ULOCK, 100);
+  sigqueue(getppid(), SIGRTMIN+5, {0});
+  exit(0);
+}
+
+#endif
+
+int main(int argc, char* argv[])
+{
+ #ifdef  __linux__
+  sigset_t sigSetPrint;
+  if (sigemptyset(&sigSetPrint) != 0) {
+    puts("sigemtyset error");
+    exit(-1);
+  }
+  if (sigaddset(&sigSetPrint, SIGRTMIN+10) != 0) {
+    puts("siaddset error");
+    exit(-2);
+  }
+  struct sigaction actOnPrint;
+  actOnPrint.sa_flags = SA_SIGINFO |SA_RESTART;
+  actOnPrint.sa_mask = sigSetPrint;
+  actOnPrint.sa_sigaction = emptyHandle;
+  if (sigaction(SIGRTMIN+10, &actOnPrint, 0) != 0) {
+    puts("sigaction error");
+    exit(-2);
+  }
+
+  sigset_t sigSetTerm;
+  if (sigemptyset(&sigSetPrint) != 0) {
+    puts("sigemtyset error");
+    exit(-1);
+  }
+  if (sigaddset(&sigSetPrint, SIGTERM) != 0) {
+    puts("sigaddset error");
+    exit(-2);
+  }
+  struct sigaction actOnTerm;
+  actOnTerm.sa_flags = SA_SIGINFO |SA_RESTART;
+  actOnTerm.sa_mask = sigSetTerm;
+  actOnTerm.sa_sigaction = sigTermHandle;
+  if (sigaction(SIGTERM, &actOnTerm, 0) != 0) {
+    puts("sigaction error");
+    exit(-2);
+  }
+  siginfo_t infoPrintSig;
+  for(long i = 0; ;i++) {
+    sigwaitinfo(&sigSetPrint, &infoPrintSig);
+    if(i == 0) {
+      printf("Start process [ pid = %d ]\n", getpid());
+    } else {
+      printMessage();
+    }
+    usleep(100);
+    sigqueue(getppid(), SIGRTMIN+5, {0});
+  }
+  return 0;
+#elif _WIN32
+  if(argc != 2) {
+    printf("argc != 2. Error\n");
+    return 1;
+  }
+  HANDLE hPrintEvent;
+  if ((hPrintEvent = OpenEventA(EVENT_ALL_ACCESS, FALSE, argv[1])) == 0) {
+    printf("OpenEvent() error. last error: ", GetLastError());
+    exit(-1);
+  }
+  HANDLE hManagerEvent;
+  if ((hManagerEvent = OpenEventA(EVENT_ALL_ACCESS, FALSE, "manager_event")) == 0) {
+    printf("OpenEvent() error. last error: ", GetLastError());
+    exit(-1);
+  }
+  HANDLE hTermEvent;
+  char termEventName[250];
+  strcpy(termEventName, argv[1]);
+  strcat(termEventName, "XXX");
+  if ((hTermEvent = OpenEventA(EVENT_ALL_ACCESS, true, termEventName)) == 0) {
+    printf("OpenEvent() error. last error = ", GetLastError());
+    exit(-1);
+  }
+  for(long i=0;;i++) {
+    WaitForSingleObject(hPrintEvent,INFINITE);
+    if(WaitForSingleObject(hTermEvent,0) == WAIT_OBJECT_0) {
+      if(SetEvent(hManagerEvent) == 0) {
+        printf("SetEvent error Manager. last error = %d\n",GetLastError());
+        exit(-2);
+      }
+      CloseHandle(hTermEvent);
+      return 0;
+    }
+    if(i == 0 ) {
+      printf("process [ pid = %d ] started ...\n", GetCurrentProcessId());
+    }
+    printMessage();
+    if(SetEvent(hManagerEvent) == 0) {
+      printf("SetEvent error. last error = %d\n",GetLastError());
+      exit(-2);
+    }
+  }
+
+    return 0;
+#endif
+}
+void printMessage() {
+
+    printf("%c", 'm');
+    printf("%c", 'y');
+    printf("%c", ' ');
+    printf("%c", 'p');
+    printf("%c", 'i');
+    printf("%c", 'd');
+    printf("%c", ' ');
+    printf("%c", '=');
+    printf("%c", ' ');
+#ifdef  __linux__
+    printf("%d", getpid());
+#elif _WIN32
+    printf("%d", GetCurrentProcessId());
+#endif
+    printf("%c", ' ');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '.');
+    printf("%c", '\n');
+}
+
---
+++ lab2/processmanager.cpp
@@ -0,0 +1,159 @@
+#include "processmanager.h"
+#include <iostream>
+#ifdef  __linux__
+#include <signal.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#elif _WIN32
+#include <cmath>
+#endif
+
+ProcessManager::ProcessManager(){}
+
+void ProcessManager::addProcess(std::string pathname)
+{
+ #ifdef  __linux__
+  try {
+    ProcessInfo tempProc;
+    tempProc.createAndExec(pathname);
+    mListProc.push_back(tempProc);
+    mPriorityProcess = mListProc.begin();
+  } catch(ProcessInfo::Exception exc) {
+    throw ProcessManager::Exception(exc.getInfo());
+  }
+ #elif _WIN32
+  SECURITY_ATTRIBUTES secAttr;
+  secAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
+  secAttr.bInheritHandle = true;
+  secAttr.lpSecurityDescriptor = 0;
+  char eventName[20];
+  sprintf(eventName, "%d", this->countProc()+1);
+  char cmdLine[250];
+  strcpy (cmdLine, pathname.c_str());
+  strcat(cmdLine, " ");
+  strcat(cmdLine, eventName);
+  HANDLE hEventPrint;
+  if((hEventPrint=CreateEventA(&secAttr, false, false, eventName)) == 0) {
+    std::cerr<<"Exc. last error = "<<GetLastError()<<std::endl;
+    throw ProcessManager::Exception ("addProcess exception");
+  }
+  mEventsPrint.push_back(hEventPrint);
+  mPriorityProcess = mEventsPrint.end();
+  mPriorityProcess--;
+
+  strcat(eventName, "XXX");
+  HANDLE hEventTerm;
+  if((hEventTerm=CreateEventA(&secAttr, false, false, eventName)) == 0) {
+    std::cerr<<"Exc2. last error = "<<GetLastError()<<std::endl;
+    throw ProcessManager::Exception ("addProcess exception");
+  }
+  mEventsTerm.push_back(hEventTerm);
+
+  ProcessInfo tempProc;
+  try {
+    tempProc.createAndExec(cmdLine);
+  } catch (ProcessInfo::Exception exc) {
+    throw ProcessManager::Exception(exc.getInfo());
+  }
+  mListProc.push_back(tempProc);
+ #endif
+}
+
+void ProcessManager::termAll()
+{
+ #ifdef _WIN32
+  auto itEventPrint = mEventsPrint.begin();
+  auto itEventTerm = mEventsTerm.begin();
+ #endif
+  auto itProc = mListProc.begin();
+  for(;itProc != mListProc.end();itProc++) {
+    try {
+      itProc->terminate();
+     #ifdef _WIN32
+      CloseHandle(*itEventPrint);
+      CloseHandle(*itEventTerm);
+      itEventPrint++;
+      itEventTerm++;
+     #endif
+    } catch (ProcessInfo::Exception exc) {
+      throw ProcessManager::Exception (exc.getInfo());
+    }
+  }
+}
+
+int ProcessManager::countProc()
+{
+    return mListProc.size();
+}
+
+int ProcessManager::termLast()
+{
+  if(mListProc.empty()) {
+    return 0;
+  }
+  ProcessInfo termProc = mListProc.back();
+  try {
+   #ifdef _WIN32
+    if(mPriorityProcess + 1 == mEventsPrint.end()) {
+      mPriorityProcess = mEventsPrint.begin();
+    }
+    SetEvent(mEventsTerm.back());
+    SetEvent(mEventsPrint.back());
+    CloseHandle(mEventsPrint.back());
+    mEventsPrint.pop_back();
+    mEventsTerm.pop_back();
+    mListProc.pop_back();
+   #elif  __linux__
+    if(mPriorityProcess + 1 == mListProc.end()) {
+      mPriorityProcess = mListProc.begin();
+    }
+    mListProc.back().terminate();
+    int exitCode;
+    waitpid(termProc.getPid(), &exitCode, WNOHANG);
+    mListProc.pop_back();
+   #endif
+    return termProc.getPid();
+   } catch (ProcessInfo::Exception exc) {
+    throw (exc.getInfo());
+   }
+}
+
+void ProcessManager::startManager()
+{
+  if(this->isEmpty()){
+    return;
+  }
+ #ifdef  __linux__
+  usleep(20000);
+  if(sigqueue(mPriorityProcess->getPid(), SIGRTMIN+10, {0})<0) {
+    std::cerr<<"sigqueue error. errno = "<<errno<<std::endl;
+    throw ProcessManager::Exception("sigqueue error");
+  }
+  if(++mPriorityProcess == mListProc.end()) {
+  mPriorityProcess = mListProc.begin();
+  }
+ #elif _WIN32
+  Sleep(20);
+  if(SetEvent(*mPriorityProcess) == 0) {
+    std::cerr<<"SetEvent error. last error = "<< GetLastError()<<std::endl;
+  }
+  if(++mPriorityProcess == mEventsPrint.end()) {
+    mPriorityProcess = mEventsPrint.begin();
+  }
+ #endif
+}
+
+bool ProcessManager::isEmpty()
+{
+    return mListProc.empty();
+}
+
+ProcessManager::Exception::Exception(std::string info)
+{
+    Exception::info=info;
+}
+
+std::string ProcessManager::Exception::getInfo()
+{
+    return Exception::info;
+}
